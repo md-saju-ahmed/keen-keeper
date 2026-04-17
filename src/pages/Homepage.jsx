190b9dcbe8064328124408d806b2fe193c362207
md-saju-ahmed <hellomdsajuahmed@gmail.com>
2026-04-17 21:27:10 +0600

    added Homepage.jsx

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import Banner from "../components/homepage/Banner";
+import Friends from "../components/homepage/Friends";
+
+const Homepage = () => {
+  return (
+    <>
+      <Banner />
+      <Friends />
+    </>
+  );
+};
+
+export default Homepage;
